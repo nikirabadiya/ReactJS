564eae9feabb909f00c51d3a0e6fb14326d462f7
Nikita Rabadiya <nikitarabadiya270@gmail.com>
2025-08-12 19:20:35 +0530

    Small changes

--- 12-mega-blog/src/components/Login.jsx
+++ 12-mega-blog/src/components/Login.jsx
@@ -39,7 +39,6 @@ function Login() {
 				<h2 className="text-center text-2xl font-bold leading-tight">
 					Sign in to your account
 				</h2>
-				{error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 				<form onSubmit={handleSubmit(login)} className="mt-8">
 					<div className="space-y-5">
 						<Input
@@ -68,6 +67,7 @@ function Login() {
 						</Button>
 					</div>
 				</form>
+				{error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 				<p className="mt-4 text-center text-base text-black/60">
 					Don&apos;t have any account?&nbsp;
 					<Link
--- 12-mega-blog/src/components/Signup.jsx
+++ 12-mega-blog/src/components/Signup.jsx
@@ -39,11 +39,17 @@ function Signup() {
 				<h2 className="text-center text-2xl font-bold leading-tight">
 					Sign up to create account
 				</h2>
-				{error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 				<form onSubmit={handleSubmit(signup)} className="mt-8">
 					<div className="space-y-5">
 						<Input
 							label="Full Name: "
+							placeholder="Enter your full name"
+							{...register("name", {
+								required: true,
+							})}
+						/>
+						<Input
+							label="Email: "
 							placeholder="Enter your email"
 							type="email"
 							{...register("email", {
@@ -68,6 +74,7 @@ function Signup() {
 						</Button>
 					</div>
 				</form>
+				{error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 				<p className="mt-4 text-center text-base text-black/60">
 					Already have an account?&nbsp;
 					<Link
