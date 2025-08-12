25d03684b3429efc4716bbaa9f3f47dbd25eee14
Nikita Rabadiya <nikitarabadiya270@gmail.com>
2025-08-12 15:54:07 +0530

    Added from for the blog content

--- 12-mega-blog/src/components/index.js
+++ 12-mega-blog/src/components/index.js
@@ -7,6 +7,11 @@ import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
 import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
+import Protected from "./AuthLayout";
+import RTE from "./RTE";
+import PostForm from "./PostForm/PostForm";
 
 export {
 	Header,
@@ -18,4 +23,9 @@ export {
 	Input,
 	Select,
 	PostCard,
+	Login,
+	Signup,
+	Protected,
+	RTE,
+	PostForm,
 };
